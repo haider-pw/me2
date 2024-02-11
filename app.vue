6408034cd755644031a27891bbd1fd569ce31638
Syed Haider Hassan <haideritx@gmail.com>
2024-02-11 08:02:41 +0500

    fix: reverting back insights

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
   <div>
-    <SpeedInsights />
     <NuxtWelcome />
   </div>
 </template>
-
-<script setup lang="ts">
-import { SpeedInsights } from '@vercel/speed-insights/vue';
-</script>
