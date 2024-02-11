566c0a0d42f21043a9b507c99a5504e208acb080
Syed Haider Hassan <haideritx@gmail.com>
2024-02-11 06:10:32 +0500

    feat(insight): vercel page speed insight

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <SpeedInsights />
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/vue';
+</script>
