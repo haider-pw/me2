093c431c86641572f3347a0b8080b438a5158076
Syed Haider Hassan <haideritx@gmail.com>
2024-03-02 19:26:53 +0500

    feat: implemented latest blog posts on home page, fetches data from dev.to

---
+++ components/Blog/Latest.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="flex flex-col gap-16">
+    <h2 class="text-2xl font-semibold text-zinc-800">
+      Latest on my blog
+    </h2>
+    <article class="group relative flex flex-col items-start" v-for="post in posts" :key="post.id">
+      <h2 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+        <div
+            class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
+        <NuxtLink :href="`/blog/${post.slug}`"><span
+            class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span><span
+            class="relative z-10">{{ post.title }}</span>
+        </NuxtLink>
+      </h2>
+      <time
+          class="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5"
+          datetime="2022-09-05">
+        <span class="absolute inset-y-0 left-0 flex items-center" aria-hidden="true">
+          <span class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
+        </span>
+        {{ post.publishedDateTime }}
+      </time>
+      <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400" v-html="post.description"/>
+      <div aria-hidden="true"
+           class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500">Read article
+        <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="ml-1 h-4 w-4 stroke-current">
+          <path d="M6.75 5.75 9.25 8l-2.5 2.25" stroke-width="1.5" stroke-linecap="round"
+                stroke-linejoin="round">
+          </path>
+        </svg>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script setup>
+const {
+  data: posts,
+  pending,
+  error
+} = await useAsyncData('postsOrPost', async () => await fetchPostsOrPost({
+  isOverview: true,
+  perPage: 2
+}), {
+  transform: (data) => transformDevToPostLists(data, true)
+});
+
+console.log('dataWouldBe', posts);
+
+</script>
---
+++ utils/index.ts
@@ -0,0 +1,61 @@
+import {SymbolKind} from "vscode-languageserver-types";
+import Number = SymbolKind.Number;
+
+export const fetchPostsOrPost = async ({isOverview, postSlug = null, perPage = null}) => {
+    const $blog = useRuntimeConfig().public.blog;
+    if (isOverview) {
+        // Fetch all posts for the blog overview
+        let url = `https://dev.to/api/articles?username=${$blog.user}`;
+
+        if (perPage && typeof perPage === 'number') {
+            url += `&per_page=${perPage}`
+        }
+
+        return $fetch(url)
+    } else {
+        // Fetch a single post by slug
+        return $fetch(`https://dev.to/api/articles/${$blog.user}/${postSlug}`)
+    }
+};
+
+const formatDateTime = (timestamp, humanReadable = false) => {
+    try {
+        const dateObj = new Date(timestamp);
+
+        // Check if date is valid
+        if (isNaN(dateObj.getTime())) {
+            throw new Error("Invalid date");
+        }
+
+        if (humanReadable) {
+            // Format the date in a human-readable form
+            const options = {year: 'numeric', month: 'long', day: 'numeric'};
+            return dateObj.toLocaleDateString('en-US', options);
+        }
+
+        // Extracting the date in YYYY-MM-DD format
+        const date = dateObj.toISOString().split('T')[0];
+
+        // Extracting the year
+        const year = dateObj.getFullYear();
+
+        return {date, year};
+    } catch (error) {
+        console.error("Error parsing timestamp:", error.message);
+        // Return null or some default value to indicate failure
+        return {date: null, year: null};
+    }
+}
+
+export const transformDevToPostLists = (data, humanReadableDateTime = false) => {
+    if (Array.isArray(data)) {
+        return data.map((post) => {
+            return {
+                ...post,
+                slug: post.path.split('/').pop(),
+                publishedDateTime: formatDateTime(post.published_timestamp, humanReadableDateTime)
+            }
+        })
+    }
+    return data;
+}
